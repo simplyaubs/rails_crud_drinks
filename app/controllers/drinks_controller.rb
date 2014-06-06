d2889ef906d6ec9167b721fd587e6d44ac4410fd
Aubrey Howell <simplyaubs@gmail.com>
2014-06-06 12:25:41 -0600

    User can update a drink from list.

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -18,6 +18,17 @@ class DrinksController < ApplicationController
     @drink = Drink.find(params[:id])
   end
 
+  def edit
+    @drink = Drink.find(params[:id])
+  end
+
+  def update
+    @drink = Drink.find(params[:id])
+    @drink.update_attributes!(drink_params)
+
+    redirect_to drink_path(@drink)
+  end
+
   private
   def drink_params
     params.require(:drink).permit(:brand, :flavor)
--- spec/features/drinks_spec.rb
+++ spec/features/drinks_spec.rb
@@ -11,4 +11,24 @@ feature 'CRUD favorite drinks' do
     expect(page).to have_content 'Coke'
     expect(page).to have_content 'Cherry'
   end
+
+  scenario 'User can update a drink' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a drink'
+    fill_in 'Brand', with: 'Coke'
+    fill_in 'Flavor', with: 'Cherry'
+    click_on 'Add drink'
+    expect(page).to have_content 'Coke'
+    expect(page).to have_content 'Cherry'
+    click_on 'Coke'
+    click_on 'Edit'
+    fill_in 'Brand', with: 'Sprite'
+    fill_in 'Flavor', with: 'Lemon'
+    click_on 'Update drink'
+    expect(page).to have_content 'Sprite'
+    expect(page).to have_content 'Lemon'
+    expect(page).to_not have_content 'Coke'
+    expect(page).to_not have_content 'Cherry'
+  end
 end
